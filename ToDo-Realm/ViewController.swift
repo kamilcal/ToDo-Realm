6b7ba4d343f7069c0a6167a652ae3351d5c403c5
Kamil Çal <kamilcaal@gmail.com>
2022-12-13 00:13:36 +0300

    Initial commit

---
+++ ToDo-Realm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo-Realm
+//
+//  Created by kamilcal on 13.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
